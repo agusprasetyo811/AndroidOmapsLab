93ce7f98853d28ffe8fda58cbabd5b2f6a5989bd
Agus Prasetyo <agusprasetyo811@gmail.com>
2013-06-29 05:08:26 +0700

    add img library from server

--- src/com/omap/lab/connection/HTTPCon.java
+++ src/com/omap/lab/connection/HTTPCon.java
@@ -5,14 +5,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.UnknownHostException;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.StrictMode;
 import android.util.Log;
 
@@ -28,12 +38,13 @@ import android.util.Log;
 public class HTTPCon {
 
 	/**
-	 * (OMAPS-LAB) Retrive data from server with GET method 
+	 * (OMAPS-LAB) Retrive data from server with GET method
+	 * 
 	 * @param url
 	 * @return {@link String}
 	 * 
 	 */
-	
+
 	public static String GET(String url) {
 		Log.i("HTTPCON", url);
 		HttpClient client = new DefaultHttpClient();
@@ -53,11 +64,39 @@ public class HTTPCon {
 		return getResponse;
 	}
 
+	/**
+	 * (OMAPS-LAB) Retrive data from server with POST method
+	 * 
+	 * @param url
+	 * @param nameValuePairs
+	 * @return {@link String}
+	 * 
+	 */
+
+	public static String POST(String url,
+			List<? extends NameValuePair> nameValuePairs) {
+		Log.i("HTTPCON", url);
+		String getResponse = null;
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(url);
+
+		// Sending to server
+		try {
+			post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			HttpResponse response = client.execute(post);
+			getResponse = HTTPCon.request(response).toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return getResponse;
+	}
+
 	public static String request(HttpResponse response) {
 		String result = "";
 		try {
 			InputStream is = response.getEntity().getContent();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(is));
 			StringBuilder str = new StringBuilder();
 			String line = null;
 			while ((line = reader.readLine()) != null) {
@@ -80,29 +119,50 @@ public class HTTPCon {
 	 */
 	public static void setOnHighestSDK() {
 		if (android.os.Build.VERSION.SDK_INT > 9) {
-			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
 			StrictMode.setThreadPolicy(policy);
 		}
 	}
-	
-	// Utiliy method to download image from the internet
-	// static private Bitmap downloadBitmap(String url) throws IOException {
-	// HttpUriRequest request = new HttpGet(url);
-	// HttpClient httpClient = new DefaultHttpClient();
-	// HttpResponse response = httpClient.execute(request);
-	//
-	// StatusLine statusLine = response.getStatusLine();
-	// int statusCode = statusLine.getStatusCode();
-	// if (statusCode == 200) {
-	// HttpEntity entity = response.getEntity();
-	// byte[] bytes = EntityUtils.toByteArray(entity);
-	//
-	// Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0,
-	// bytes.length);
-	// return bitmap;
-	// } else {
-	// throw new IOException("Download failed, HTTP response code "
-	// + statusCode + " - " + statusLine.getReasonPhrase());
-	// }
-	// }
+
+	public static Bitmap DownloadImage(String URL) {
+		Bitmap bitmap = null;
+		InputStream in = null;
+		try {
+			in = OpenHttpConnection(URL);
+			bitmap = BitmapFactory.decodeStream(in);
+			in.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return bitmap;
+	}
+
+	public static InputStream OpenHttpConnection(String urlString) throws IOException {
+		InputStream in = null;
+		int response = -1;
+
+		URL url = new URL(urlString);
+		URLConnection conn = url.openConnection();
+
+		if (!(conn instanceof HttpURLConnection))
+			throw new IOException("Not an HTTP connection");
+
+		try {
+			HttpURLConnection httpConn = (HttpURLConnection) conn;
+			httpConn.setAllowUserInteraction(false);
+			httpConn.setInstanceFollowRedirects(true);
+			httpConn.setRequestMethod("GET");
+			httpConn.connect();
+
+			response = httpConn.getResponseCode();
+			if (response == HttpURLConnection.HTTP_OK) {
+				in = httpConn.getInputStream();
+			}
+		} catch (Exception ex) {
+			throw new IOException("Error connecting");
+		}
+		return in;
+	}
 }
